13c2334d2d5ffd13fa3c4a736c8c553c2be8e0cd
JustSG <szymonfujawa.07@gmail.com>
2026-07-16 13:32:14 +0200

    Add age verification

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,18 @@ void main() {
   for (int i = 0; i < 3; i++) {
     System.out.println("Hello World!");
   }
+
+  Scanner scanner = new Scanner(System.in);
+  System.out.println("What is your age?");
+  //TODO: add try-catch
+  int age = scanner.nextInt();
+  scanner.close();
+
+  if(age >= 0 && age < 18){
+    System.out.println("You're a child.");
+  }else if(age >= 18){
+    System.out.println("You're an adult.");
+  }else{
+    System.out.println("What?");
+  }
 }
