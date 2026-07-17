4d82256867277cba5e4209e91e9df046a8fe8d07
KarixD2137 <karol.kopczynski7@gmail.com>
2026-07-17 13:10:52 +0200

    Add counting

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
 void main() {
-  for (int i = 0; i < 3; i++) {
-    System.out.println("Hello World!");
-  }
+    for (int i = 0; i < 3; i++) {
+        System.out.println("Hello World!");
+    }
 
-  Scanner scanner = new Scanner(System.in);
-  System.out.println("What is your age?");
-  //TODO: add try-catch
-  int age = scanner.nextInt();
-  scanner.close();
+    System.out.println();
+    for (int i = 0; i < 10; i++) {
+        System.out.println(i + 1);
+    }
+    System.out.println();
 
-  if(age >= 0 && age < 18){
-    System.out.println("You're a child.");
-  }else if(age >= 18){
-    System.out.println("You're an adult.");
-  }else{
-    System.out.println("What?");
-  }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What is your age?");
+    //TODO: add try-catch
+    int age = scanner.nextInt();
+    scanner.close();
+
+    if (age >= 0 && age < 18) {
+        System.out.println("You're a child.");
+    } else if (age >= 18) {
+        System.out.println("You're an adult.");
+    } else {
+        System.out.println("What?");
+    }
 }
